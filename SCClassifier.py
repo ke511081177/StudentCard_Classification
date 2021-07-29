793f34d33fd9bd3616ee9079150ea2153d2bad29
hank-influx <86759251+hank-influx@users.noreply.github.com>
2021-07-29 17:17:30 +0800

    Update SCClassifier.py

--- SCClassifier.py
+++ SCClassifier.py
@@ -8,26 +8,18 @@ import os
 # from loss import*
 # from func import*
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="XXXXXXX"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="subtle-fulcrum-319206-415ab8f59c71.json"
 from google.cloud import vision
 client = vision.ImageAnnotatorClient()
 import jieba
-
-
 import re
 
-
-
-folderlist = ['unsorted']
+folderlist = []
 
 folderpath = './classify'
+path = './image'
 
 school = ['大學','高中','國中','國小','二專','四技','學校','私立','公立']
-
-
-
-
-
 
 
 
@@ -40,34 +32,31 @@ class diplomaClassifier():
         self.tempSchool =''
 
     def run(self,file):
-        
-        path = './image'
         self.img = cv2.imread('{}/{}'.format(path,file))
         
         #self.img = cv2.imread('./{}'.format(file))
 
-        self.imgSave = self.img
-
+       
 
         """Detects text in the file."""
         tempImg = self.img
         
         file = re.sub('[.jpg]', '', file)
 
-
+        # 編碼用於傳輸
         success, encoded_image = cv2.imencode('.jpg', self.img)
-
         content = encoded_image.tobytes()
-        image = vision.Image(content=content)
 
+        # Call API
+        image = vision.Image(content=content)
         response = client.text_detection(image=image)
+       
         texts = response.text_annotations
-        print(file)
 
         # 沒有偵測到字詞
         if len(texts) == 0:
-
-            print('NOo text')
+            
+            print('NO text')
             os.makedirs('./{}/unsorted/{}'.format(folderpath,file))
             cv2.imwrite('{}/unsorted/{}/{}.jpg'.format(folderpath,file,file),tempImg)
             
@@ -78,18 +67,15 @@ class diplomaClassifier():
 
         jieba.load_userdict('./jiabaDictionary/school.txt')
         seg_list = jieba.cut(text)
-    
-        seg = []
-
+        
         for i in seg_list:
             if i ==' ':
                 continue
-            seg.append(i)
-
+            self.text.append(i)
+        print(self.text)
         buffer = []
-
         for c in school:
-            for i in seg:   
+            for i in self.text:   
                 if c in i:
                     buffer.append(i)
                     
@@ -106,12 +92,11 @@ class diplomaClassifier():
             for i in buffer:
                 if len(self.tempSchool) < len(i):
                     self.tempSchool = i
-            print(self.tempSchool,'!')
+            
             if len(self.tempSchool) < 4:
                 print('school: Not Found')
 
                 os.makedirs('./{}/unsorted/{}'.format(folderpath,file))
-
                 cv2.imwrite('{}/unsorted/{}/{}.jpg'.format(folderpath,file,file),tempImg)
            
             else:
@@ -126,38 +111,7 @@ class diplomaClassifier():
                 cv2.imencode('.jpg',tempImg)[1].tofile('classify/{}/{}.jpg'.format(self.tempSchool,file)) 
 
     
-    def stampCut(self):
-        
-        
-        
-        self.img = cv2.cvtColor(self.img,cv2.COLOR_BGR2RGB)
-        rimg = cv2.resize(self.img, (256, 256))
 
-
-        binary = np.reshape(rimg,(1,)+rimg.shape)
-        
-        mask = self.stampModel.predict(binary)
-        
-        mask = np.stack((mask,)*3, axis=-1)
-        
-        mask = np.reshape(mask,(256,256,3))
-        
-        mask = cv2.resize(mask,(self.img.shape[1],self.img.shape[0]))
-        
-        mask = mask[:,:,0]
-
-        mask = np.uint8(mask)
-        
-        self.img = cv2.add(self.img, np.zeros(np.shape(self.img), dtype=np.uint8), mask=mask)
-        self.img = cv2.cvtColor(self.img,cv2.COLOR_RGB2BGR)
-    
-
-
-
-
-        
-    
-jieba.case_sensitive = True
 
 if __name__ == '__main__':
     classifiler = diplomaClassifier()
@@ -166,9 +120,9 @@ if __name__ == '__main__':
     filelist = os.listdir(path)
 
     jieba.case_sensitive = True
-    #classifiler.run('temp.jpg')
-    for file in filelist:
-        print(file)
-        classifiler.img  = cv2.imread('{}/{}'.format(path,file))
-        classifiler.run(file)
+    classifiler.run('152.jpg')
+    # for file in filelist:
+    #     #print(file)
+    #     classifiler.img  = cv2.imread('{}/{}'.format(path,file))
+    #     classifiler.run(file)
 
